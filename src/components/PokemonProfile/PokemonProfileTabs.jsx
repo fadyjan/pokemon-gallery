075f61e05f97890db7654d24506e1999bc7d7d17
fadyjan <fady.jan95@gmail.com>
2023-05-11 02:46:15 +0330

    Create #7 PokemonProfileTabs Component

---
+++ src/components/PokemonProfile/PokemonProfileTabs.jsx
@@ -0,0 +1,105 @@
+import * as React from "react";
+import PropTypes from "prop-types";
+import SwipeableViews from "react-swipeable-views";
+import { useTheme } from "@mui/material/styles";
+import AppBar from "@mui/material/AppBar";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import module from "./PokemonProfileTabs.module.css"
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`full-width-tabpanel-${index}`}
+      aria-labelledby={`full-width-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `full-width-tab-${index}`,
+    "aria-controls": `full-width-tabpanel-${index}`,
+  };
+}
+
+export default function PokemonProfileTabs({selectedPokemon}) {
+  const theme = useTheme();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const handleChangeIndex = (index) => {
+    setValue(index);
+  };
+
+  return (
+    //like container div
+    <>
+      {selectedPokemon && (
+        <Box sx={{ bgcolor: "transparent", width: 500 }}>
+          <AppBar position="static" id={module.AppBarTabs}>
+            <Tabs
+              value={value}
+              onChange={handleChange}
+
+
+              variant="fullWidth"
+              aria-label="full width tabs example"
+            >
+              <Tab label="STATS" {...a11yProps(0)} />
+              <Tab label="MOVES" {...a11yProps(1)} />
+              <Tab label="ABILITIES" {...a11yProps(2)} />
+            </Tabs>
+          </AppBar>
+
+          <SwipeableViews
+            axis={theme.direction === "rtl" ? "x-reverse" : "x"}
+            index={value}
+            onChangeIndex={handleChangeIndex}
+          >
+            <TabPanel value={value} index={0} dir={theme.direction}>
+              {selectedPokemon.stats.map((state) => {
+                return( <span key={state.stat.name+selectedPokemon.id}>{state.stat.name +'|'+ state.base_stat }<br></br></span>)
+              })}
+            </TabPanel>
+            <TabPanel value={value} index={1} dir={theme.direction}>
+            {selectedPokemon.moves.slice(0,10).map((move) => {
+                return( <span key={move.move.name+selectedPokemon.id}>{move.move.name}<br></br></span>)
+              })}
+            </TabPanel>
+            <TabPanel value={value} index={2} dir={theme.direction}>
+            {selectedPokemon.abilities.map((ability) => {
+                return( <span key={ability.ability.name +selectedPokemon.id}>{ability.ability.name}<br></br></span>)
+              })}
+            </TabPanel>
+          </SwipeableViews>
+        </Box>
+      )}
+    </>
+  );
+}
+
+PokemonProfileTabs.propTypes = {
+  selectedPokemon: PropTypes.object.isRequired,
+};
