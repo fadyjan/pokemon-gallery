2677c500e2e9f119858034ca74a77e8deabca33c
fadyjan <fady.jan95@gmail.com>
2023-05-11 02:44:41 +0330

    Create #2 Project Initilization

---
+++ src/components/LoadingSpinner/LoadingSpinner.jsx
@@ -0,0 +1,9 @@
+import ClipLoader from "react-spinners/ClipLoader";
+
+const LoadingSpinner = () => {
+  return (
+    <ClipLoader color="#EF5350"  speedMultiplier = "0.5" size ="500px"/>
+  )
+}
+
+export default LoadingSpinner
