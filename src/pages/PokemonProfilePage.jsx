1bd1f1f743ef7b302bdf69404a81cb12c23cf0be
fadyjan <fady.jan95@gmail.com>
2023-05-11 02:51:10 +0330

    Create #10 PokemonProfilePage

---
+++ src/pages/PokemonProfilePage.jsx
@@ -0,0 +1,61 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import modules from "./PokemonProfilePage.module.css";
+import PokemonProfileTabs from "../components/PokemonProfile/PokemonProfileTabs";
+import LoadingSpinner from "../components/LoadingSpinner/LoadingSpinner";
+import axios from "axios";
+
+import Button from '@mui/material/Button';
+import ArrowBack from '@mui/icons-material/ArrowBack';
+import { Link } from "react-router-dom";
+
+
+
+const PokemonProfilePage = () => {
+  const [selectedPokemon, setSelectedPokemon] = useState();
+
+  const { id } = useParams();
+  console.log(id);
+  useEffect(() => {
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon/${id}`)
+      .then((response) => {
+        setSelectedPokemon(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [id]);
+  return (
+    <>
+     <Link to={`/`}>
+
+     <Button variant="contained" startIcon={<ArrowBack />}>BACK</Button>
+
+     </Link>
+
+      {selectedPokemon ? (
+        <div id={modules.PrfilePageWrapper}>
+          <div id={modules.PokemonWrapper}>
+            <img
+              src={selectedPokemon.sprites.other.dream_world.front_default}
+            ></img>
+            <div id={modules.PokemonInfoWrapper}>
+              <h1>{selectedPokemon.name}</h1>
+              <div id = {modules.abilities}>
+                {selectedPokemon.types.map((type) => (
+                  <span key={type.type.name}>{type.type.name}</span>
+                ))}
+              </div>
+            </div>
+          </div>
+          <PokemonProfileTabs selectedPokemon ={selectedPokemon}></PokemonProfileTabs>
+        </div>
+      ) : (
+        <LoadingSpinner></LoadingSpinner>
+      )}
+    </>
+  );
+};
+
+export default PokemonProfilePage;
