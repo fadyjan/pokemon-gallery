2ebfe30b1132949a14b8b7684c45fd58be6ac676
fadyjan <fady.jan95@gmail.com>
2023-05-11 02:49:47 +0330

    Create #8 HomePage

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,10 @@
+import PokemonGallery from "../components/PokemonGallery/PokemonGallery";
+const HomePage = () => {
+  return (
+    <>
+      <PokemonGallery></PokemonGallery>
+    </>
+  );
+};
+
+export default HomePage;
