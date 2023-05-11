20c52da875fa264ef11aa3c47d75429a313fe9f8
fadyjan <fady.jan95@gmail.com>
2023-05-11 18:27:20 +0300

    fix #4 Loading Spinner size

--- src/components/LoadingSpinner/LoadingSpinner.jsx
+++ src/components/LoadingSpinner/LoadingSpinner.jsx
@@ -2,7 +2,7 @@ import ClipLoader from "react-spinners/ClipLoader";
 
 const LoadingSpinner = () => {
   return (
-    <ClipLoader color="#EF5350"  speedMultiplier = "0.5" size ="500px"/>
+    <ClipLoader color="#EF5350"  speedMultiplier = "0.5" size ={window.innerWidth > 650 ? "500px" : "250px"} />
   )
 }
 
